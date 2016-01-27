2d5537a304a952df4a753329e2b8d03d01cdc7b8
jay whang and taylor grohs <jaywhang83@gmail.com>
2016-01-27 08:41:20 -0800

    add player two test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 function PlayerOne (playerOneMark) {
   this.playerOneMark = playerOneMark;
-}; 
+};
+
+function PlayerTwo (playerTwoMark) {
+  this.playerTwoMark = playerTwoMark;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,13 @@
 describe("PlayerOne", function(){
-  it("returns the player's mark", function() {
+  it("returns the player one's mark", function() {
     var testPlayer = new PlayerOne("X");
     expect(testPlayer.playerOneMark).to.equal("X");
+  });
+});
+
+describe("PlayerTwo", function(){
+  it("returns the player two's mark", function() {
+    var testPlayer = new PlayerTwo("O");
+    expect(testPlayer.playerTwoMark).to.equal("O");
   });
 });
