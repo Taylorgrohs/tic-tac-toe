802ce70eb38a0018c1ad10d05c08a0b8705da06a
jay whang and taylor grohs <jaywhang83@gmail.com>
2016-01-27 10:46:42 -0800

    display o and x on the board

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,28 @@ Space.prototype.markedBy = function(playerMark) {
   Player.call(this, playerMark);
   return this.playerMark;
 }
+
+
+$(document).ready(function(){
+  var x = "X";
+  var o = "O";
+  var count = 0;
+  $("button").click(function(event){
+    event.preventDefault();
+
+
+      console.log(count);
+    if (count % 2 === 0) {
+      count++;
+      $(this).text(o);
+      //console.log(count);
+    }
+    else {
+      count++;
+      $(this).text(x);
+
+    }
+
+
+  });
+});
