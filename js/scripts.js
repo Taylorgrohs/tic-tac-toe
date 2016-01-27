f186e1c72814ebeed38d56220892fbc4a9c36508
jay whang and taylor grohs <taylorgrohs@gmail.com>
2016-01-27 11:39:39 -0800

    add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -14,25 +14,37 @@ Space.prototype.markedBy = function(playerMark) {
 
 
 $(document).ready(function(){
-  var x = "X";
-  var o = "O";
-  var count = 0;
-  $("button").click(function(event){
+  $("form").submit(function(event){
+    var player1 = $("input#player1Icon").val();
+    var player2 = $("input#player2Icon").val();
+    var inputtedPlayer1 = new Player(player1);
+    var inputtedPlayer2 = new Player(player2);
     event.preventDefault();
+    var count = 0;
+    $("button.game").click(function(event){
+      event.preventDefault();
 
 
-      console.log(count);
-    if (count % 2 === 0) {
-      count++;
-      $(this).text(o);
-      //console.log(count);
-    }
-    else {
-      count++;
-      $(this).text(x);
+        console.log(count);
+      if (count % 2 === 0) {
+        count++;
+        $(this).text(inputtedPlayer2.playerMark);
+        //console.log(count);
+      }
+      else {
+        count++;
+        $(this).text(inputtedPlayer1.playerMark);
 
-    }
+      }
+
+    console.log(inputtedPlayer1);
+    console.log(inputtedPlayer2);
+  });
 
 
+
+  });
+  $("button.clear-board").click(function(){
+    $("button.game").empty();
   });
 });
