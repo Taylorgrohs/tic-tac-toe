b33a10b0091072f4bfa157c462da826c5db7cef7
jay whang and taylor grohs <jaywhang83@gmail.com>
2016-01-27 13:23:49 -0800

    Finish jquery tic tac toe

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,30 @@ function Player (playerMark) {
   this.playerMark = playerMark;
 };
 
-function Space (x, y) {
-  this.xCoordinate = x;
-  this.yCoordinate = y;
-};
+  //
+  //
+  // $(document).ready(function(){
+  //   $("form").submit(function(event){
+  //     var player1 = $("input#player1Icon").val();
+  //     var player2 = $("input#player2Icon").val();
+  //       var inputtedPlayer1 = new Player(player1);
+  //       var inputtedPlayer2 = new Player(player2);
+  //
+  //       $("button.game").click(function(event){
+  //       this.id
+  //       });
+  //       event.preventDefault();
+  //   });
+  // });
+  //
+  //
 
-Space.prototype.markedBy = function(playerMark) {
-  Player.call(this, playerMark);
-  return this.playerMark;
-}
+
+
+
+
+
+
 
 
 $(document).ready(function(){
@@ -23,25 +38,66 @@ $(document).ready(function(){
     var count = 0;
     $("button.game").click(function(event){
       event.preventDefault();
-
-
         console.log(count);
+
+        // if ($("#1").hasClass("player1") && $("#2").hasClass("player1") && $("#3").hasClass("player1") || $("#4").hasClass("player1") && $("#5").hasClass("player1") && $("#6").hasClass("player1") ||
+        // $("#7").hasClass("player1") && $("#8").hasClass("player1") && $("#9").hasClass("player1") ||
+        // $("#1").hasClass("player1") && $("#4").hasClass("player1") && $("#7").hasClass("player1") ||
+        // $("#2").hasClass("player1") && $("#5").hasClass("player1") && $("#8").hasClass("player1") || $("#3").hasClass("player1") && $("#6").hasClass("player1") &&
+        // $("#9").hasClass("player1") ||
+        // $("#1").hasClass("player1") && $("#5").hasClass("player1") &&
+        // $("#9").hasClass("player1") ||
+        // $("#3").hasClass("player1") && $("#5").hasClass("player1") &&
+        // $("#7").hasClass("player1")) {
+        //   alert("Player1 has won!!!!")
+        // // } else if ($("#1").hasClass("player2") && $("#2").hasClass("player2") && $("#3").hasClass("player2") || $("#4").hasClass("player2") && $("#5").hasClass("player2") && $("#6").hasClass("player2") ||
+        // $("#7").hasClass("player2") && $("#8").hasClass("player2") && $("#9").hasClass("player2") ||
+        // $("#1").hasClass("player2") && $("#4").hasClass("player2") && $("#7").hasClass("player2") ||
+        // $("#2").hasClass("player2") && $("#5").hasClass("player2") && $("#8").hasClass("player2") || $("#3").hasClass("player2") && $("#6").hasClass("player2") &&
+        // $("#9").hasClass("player2") ||
+        // $("#1").hasClass("player2") && $("#5").hasClass("player2") &&
+        // $("#9").hasClass("player2") ||
+        // $("#3").hasClass("player2") && $("#5").hasClass("player2") &&
+        // $("#7").hasClass("player2")) {
+        //   alert("Player2 has won!!!!")
+        // }
       if (count % 2 === 0) {
         count++;
         $(this).text(inputtedPlayer2.playerMark);
+        $(this).addClass("player2");
+        if ($("#1").hasClass("player2") && $("#2").hasClass("player2") && $("#3").hasClass("player2") || $("#4").hasClass("player2") && $("#5").hasClass("player2") && $("#6").hasClass("player2") ||
+        $("#7").hasClass("player2") && $("#8").hasClass("player2") && $("#9").hasClass("player2") ||
+        $("#1").hasClass("player2") && $("#4").hasClass("player2") && $("#7").hasClass("player2") ||
+        $("#2").hasClass("player2") && $("#5").hasClass("player2") && $("#8").hasClass("player2") || $("#3").hasClass("player2") && $("#6").hasClass("player2") &&
+        $("#9").hasClass("player2") ||
+        $("#1").hasClass("player2") && $("#5").hasClass("player2") &&
+        $("#9").hasClass("player2") ||
+        $("#3").hasClass("player2") && $("#5").hasClass("player2") &&
+        $("#7").hasClass("player2")) {
+          alert("Player2 has won!!!!")
+        }
         //console.log(count);
       }
       else {
         count++;
         $(this).text(inputtedPlayer1.playerMark);
+        $(this).addClass("player1");
+        if ($("#1").hasClass("player1") && $("#2").hasClass("player1") && $("#3").hasClass("player1") || $("#4").hasClass("player1") && $("#5").hasClass("player1") && $("#6").hasClass("player1") ||
+        $("#7").hasClass("player1") && $("#8").hasClass("player1") && $("#9").hasClass("player1") ||
+        $("#1").hasClass("player1") && $("#4").hasClass("player1") && $("#7").hasClass("player1") ||
+        $("#2").hasClass("player1") && $("#5").hasClass("player1") && $("#8").hasClass("player1") || $("#3").hasClass("player1") && $("#6").hasClass("player1") &&
+        $("#9").hasClass("player1") ||
+        $("#1").hasClass("player1") && $("#5").hasClass("player1") &&
+        $("#9").hasClass("player1") ||
+        $("#3").hasClass("player1") && $("#5").hasClass("player1") &&
+        $("#7").hasClass("player1")) {
+          alert("Player1 has won!!!!")
+        }
 
       }
-
     console.log(inputtedPlayer1);
     console.log(inputtedPlayer2);
   });
-
-
 
   });
   $("button.clear-board").click(function(){
--- spec/specs.js
+++ spec/specs.js
@@ -11,19 +11,3 @@ describe("Player", function(){
     expect(testPlayer.playerMark).to.equal("O");
   });
 });
-
-describe("Space", function(){
-  it("reutnrs the player's mark", function(){
-    var testSpace = new Space(1,2);
-    expect(testSpace.xCoordinate).to.equal(1);
-  });
-  it("returns the player's mark", function() {
-    var testSpace = new Space(1,2);
-    expect(testSpace.yCoordinate).to.equal(2);
-   });
-   it("lets a player mark a space", function() {
-     var testPlayer = new Player("X");
-     var testSpace = new Space(1, 2);
-     expect(testSpace.markedBy(testPlayer.playerMark)).to.equal(testPlayer.playerMark);
-   });
-});
